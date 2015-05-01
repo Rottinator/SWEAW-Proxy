4d0714fba8e043582949a998bf8e7b15d6d30132
Christoph Rothmeier <rotti0495@gmail.com>
2015-05-01 22:18:45 +0200

    Billiglösung implementiert

--- Client/main.cpp
+++ Client/main.cpp
@@ -1,10 +1,151 @@
 #include <iostream>
 #include <cstdio>
+#include <cstdlib>
 #include "../../SocketLibrary/SocketLibrary/SocketLibrary.h"
+
+#define GAME_PORT 1234
+
+#define SERVER_BROADCAST_PORT 5001
+#define SERVER_DATA_PORT 5002
 
 using namespace std;
 
+UdpClient* localBroadcastClient;
+UdpClient* localDataClient;
+TcpClient* serverDataClient;
+TcpClient* serverBroadcastClient;
+
+char serverIp[30];
+char localIp[30];
+
+void ReceiveLocalBroadcast(char*, int, char*, unsigned short);
+void ReceiveLocalData(char*, int, char*, unsigned short);
+
+void ReceiveServerBroadcast(char*, int);
+void ReceiveServerData(char*, int);
+
+void InitLocalBroadcastClient();
+void InitLocalDataClient();
+void InitServerBroadcastClient();
+void InitServerDataClient();
+
 int main()
 {
+	//printf("Server-IP: ");
+	//scanf("%s", &serverIp);
+	//getchar();
+
+	//printf("Lokale-IP: ");
+	//scanf("%s", &localIp);
+	//getchar();	
+
+	InitSocketLibrary();
+
+	InitLocalBroadcastClient();
+	InitLocalDataClient();
+	InitServerBroadcastClient();
+	InitServerDataClient();	
 	
+	CleanupSocketLibrary();
+
+	while(true)
+	{
+		SleepMilliseconds(1000);
+	}
+
+	return 0;
+}
+
+void ReceiveLocalBroadcast(char* data, int length, char* address, unsigned short port)
+{
+	serverBroadcastClient->Send(data, length);
+	delete data;
+	delete address;
+}
+
+void ReceiveLocalData(char* data, int length, char* address, unsigned short port)
+{
+	serverDataClient->Send(data, length);
+	delete data;
+	delete address;
+}
+
+void ReceiveServerBroadcast(char* data, int length)
+{
+	localDataClient->SendTo(data, length, IpAddress.Broadcast, GAME_PORT);
+	delete data;
+}
+
+void ReceiveServerData(char* data, int length)
+{
+	localDataClient->SendTo(data, length, localIp, GAME_PORT);
+	delete data;
+}
+
+void InitLocalBroadcastClient()
+{
+	localBroadcastClient = new UdpClient();
+	bool success = localBroadcastClient->Bind(IpAddress.Any, GAME_PORT);
+	if(success) 
+	{
+		printf("Local-Broadcast-Client erfolgreich initialisert\n");
+		localBroadcastClient->StartReceiveData(ReceiveLocalBroadcast);
+	}
+	else
+	{
+		printf("Fehler beim Initialisieren des Local-Broadcast-Client\n");
+		system("Pause");
+		exit(0);
+	}
+}
+
+void InitLocalDataClient()
+{
+	localDataClient = new UdpClient();
+	bool success = localDataClient->Bind(IpAddress.Any, 0);
+	if(success) 
+	{
+		printf("Local-Data-Client erfolgreich initialisert\n");
+		localDataClient->StartReceiveData(ReceiveLocalData);
+	}
+	else
+	{
+		printf("Fehler beim Initialisieren des Local-Data-Clients\n");
+		system("Pause");
+		exit(0);
+	}
+}
+
+void InitServerBroadcastClient()
+{
+	serverBroadcastClient = new TcpClient();
+	bool success = serverBroadcastClient->Connect("192.168.2.143", 5001);
+	if(success) 
+	{
+		printf("Broadcast-Client erfolgreich mit dem Server verbunden\n");
+		serverBroadcastClient->StartReceiveData(ReceiveServerBroadcast);
+	}
+	else
+	{
+		printf("Fehler beim Verbinden des Broadcast-Clients zum Server\n");
+		system("Pause");
+		exit(0);
+	}
+}
+
+void InitServerDataClient()
+{
+	serverDataClient = new TcpClient();
+	bool success = serverDataClient->Connect("127.0.0.1", SERVER_DATA_PORT);
+	if(success) 
+	{
+		printf("Data-Client erfolgreich mit dem Server verbunden\n");
+		serverDataClient->StartReceiveData(ReceiveServerData);
+	}
+	else
+	{
+		printf("Fehler beim Verbinden des Data-Clients zum Server\n");
+		system("Pause");
+		exit(0);
+	}
 }
--- Server/main.cpp
+++ Server/main.cpp
@@ -1,10 +1,141 @@
 #include <iostream>
+#include <cstdlib>
 #include <cstdio>
+
 #include "../../SocketLibrary/SocketLibrary/SocketLibrary.h"
+
+#define BROADCAST_PORT 5001
+#define DATA_PORT 5002
 
 using namespace std;
 
+TcpListener* broadcastListener;
+TcpListener* dataListener;
+
+TcpClient* broadcastClient1 = NULL;
+TcpClient* broadcastClient2 = NULL;
+TcpClient* dataClient1 = NULL;
+TcpClient* dataClient2 = NULL;
+
+void InitBroadcastListener();
+void InitDataListener();
+
+void OnBroadcastClientAccepted(TcpClient*);
+void OnBroadcastReceive1(char*, int);
+void OnBroadcastReceive2(char*, int);
+
+void OnDataClientAccepted(TcpClient*);
+void OnDataReceive1(char*, int);
+void OnDataReceive2(char*, int);
+
 int main()
 {
+	InitSocketLibrary();
 	
+	InitBroadcastListener();
+	InitDataListener();
+
+	while(true)
+	{
+		SleepMilliseconds(1000);
+	}
+}
+
+void InitBroadcastListener()
+{
+	broadcastListener = new TcpListener();
+	bool success = broadcastListener->Bind(IpAddress.Any, BROADCAST_PORT);
+	if(success == true) 
+	{
+		printf("Broadcast-Listener erfolgreich initialisiert\n");
+		broadcastListener->StartListening(OnBroadcastClientAccepted);
+	}
+	else
+	{
+		printf("Fehler beim initialisieren des Broadcast-Listeners\n");
+	}
+}
+
+void InitDataListener()
+{
+	dataListener = new TcpListener();
+	bool success = dataListener->Bind(IpAddress.Any, DATA_PORT);
+	if(success == true) 
+	{
+		printf("Data-Listener erfolgreich initialisiert\n");
+		dataListener->StartListening(OnDataClientAccepted);
+	}
+	else
+	{
+		printf("Fehler beim initialisieren des Data-Listeners\n");
+	}
+}
+
+
+void OnBroadcastClientAccepted(TcpClient* client)
+{
+	if(broadcastClient1 == NULL) 
+	{
+		printf("Broadcast-Client-1 hat sich verbunden\n");
+		broadcastClient1 = client;
+		client->StartReceiveData(OnBroadcastReceive1);
+	}
+	else if(broadcastClient2 == NULL)
+	{
+		printf("Broadcast-Client-2 hat sich verbunden\n");
+		broadcastClient2 = client;
+		client->StartReceiveData(OnBroadcastReceive2);
+	}
+}
+
+void OnBroadcastReceive1(char* data, int length)
+{
+	if(broadcastClient2 != NULL)
+	{
+		broadcastClient2->Send(data, length);
+	}
+
+	delete data;
+}
+
+void OnBroadcastReceive2(char* data, int length)
+{
+	broadcastClient1->Send(data, length);
+
+	delete data;
+}
+
+
+void OnDataClientAccepted(TcpClient* client)
+{
+	
+	if(dataClient1 == NULL)
+	{
+		printf("Data-Client-1 hat sich verbunden\n");
+		dataClient1 = client;
+		client->StartReceiveData(OnDataReceive1);
+	}
+	else if(dataClient2 == NULL)
+	{
+		printf("Data-Client-2 hat sich verbunden\n");
+		dataClient2 = client;
+		client->StartReceiveData(OnDataReceive2);
+	}
+}
+
+void OnDataReceive1(char* data, int length)
+{
+	if(dataClient2 != NULL)
+	{
+		dataClient2->Send(data, length);
+	}
+
+	delete data;
+}
+
+void OnDataReceive2(char* data, int length)
+{
+	dataClient1->Send(data, length);
+
+	delete data;
 }
